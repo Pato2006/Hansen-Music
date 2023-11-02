3f181eb1afb1e88b5b2e60a425fdae30c3c39380
Patricio_Zarate <patrizarate1@gmail.com>
2023-11-02 14:11:22 -0300

    Borro todo de vuelta

--- PHP/env.example.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("HOST", "");
-define("DB", "");
-define("USER","");
-define("PASSWORD", "");
-
-$con = mysqli_connect(HOST, USER, PASSWORD, DB);
-
-?>
--- PHP/env.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("HOST", "localhost");
-define("DB", "hansen_music");
-define("USER","root");
-define("PASSWORD", "");
-
-$con = mysqli_connect(HOST, USER, PASSWORD, DB);
-
-?>
