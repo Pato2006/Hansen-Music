53ebdcc02e6fb4a28b3e7eec47baa70130ed7fd4
Patricio_Zarate <patrizarate1@gmail.com>
2023-11-02 17:28:24 -0300

    Base de datos con orientacion en el producto

--- hansen/db/hansen_music.sql
+++ hansen/db/hansen_music.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-11-2023 a las 17:39:05
+-- Tiempo de generación: 02-11-2023 a las 21:27:32
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.0.28
 
@@ -84,7 +84,8 @@ INSERT INTO `orientations` (`id`, `name`) VALUES
 CREATE TABLE `products` (
   `id` int(8) NOT NULL,
   `publication_id` int(8) NOT NULL,
-  `brand_id` int(8) NOT NULL
+  `brand_id` int(8) NOT NULL,
+  `orientation_id` int(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -99,22 +100,21 @@ CREATE TABLE `publications` (
   `name` varchar(50) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
   `price` int(9) DEFAULT NULL,
-  `state` enum('Nuevo','Usado') DEFAULT NULL,
-  `orientation_id` int(8) DEFAULT NULL
+  `state` enum('Nuevo','Usado') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `publications`
 --
 
-INSERT INTO `publications` (`id`, `seller_id`, `name`, `description`, `price`, `state`, `orientation_id`) VALUES
-(19, NULL, 'Guitarra Acústica', '', 500, 'Nuevo', 1),
-(20, NULL, 'Teclado Eléctrico', '', 750, 'Usado', 2),
-(21, NULL, 'Batería Electrónica', '', 1000, 'Nuevo', 3),
-(22, NULL, 'Bajo Eléctrico', '', 600, 'Nuevo', 1),
-(23, NULL, 'Saxofón Tenor', '', 800, 'Nuevo', 2),
-(24, NULL, 'Trompeta', '', 550, 'Usado', 3),
-(25, NULL, 'Guitarra zurda', '', 12000, 'Nuevo', 2);
+INSERT INTO `publications` (`id`, `seller_id`, `name`, `description`, `price`, `state`) VALUES
+(19, NULL, 'Guitarra Acústica', '', 500, 'Nuevo'),
+(20, NULL, 'Teclado Eléctrico', '', 750, 'Usado'),
+(21, NULL, 'Batería Electrónica', '', 1000, 'Nuevo'),
+(22, NULL, 'Bajo Eléctrico', '', 600, 'Nuevo'),
+(23, NULL, 'Saxofón Tenor', '', 800, 'Nuevo'),
+(24, NULL, 'Trompeta', '', 550, 'Usado'),
+(25, NULL, 'Guitarra zurda', '', 12000, 'Nuevo');
 
 -- --------------------------------------------------------
 
@@ -202,15 +202,15 @@ ALTER TABLE `orientations`
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_brand` (`brand_id`),
-  ADD KEY `FK_publications_id` (`publication_id`);
+  ADD KEY `FK_publications_id` (`publication_id`),
+  ADD KEY `FK_orientation_id` (`orientation_id`);
 
 --
 -- Indices de la tabla `publications`
 --
 ALTER TABLE `publications`
   ADD PRIMARY KEY (`id`) USING BTREE,
-  ADD KEY `vendedor` (`seller_id`),
-  ADD KEY `fk_ori` (`orientation_id`);
+  ADD KEY `vendedor` (`seller_id`);
 
 --
 -- Indices de la tabla `sends`
@@ -281,7 +281,7 @@ ALTER TABLE `status`
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
@@ -299,6 +299,7 @@ ALTER TABLE `buys`
 -- Filtros para la tabla `products`
 --
 ALTER TABLE `products`
+  ADD CONSTRAINT `FK_orientation_id` FOREIGN KEY (`orientation_id`) REFERENCES `orientations` (`id`),
   ADD CONSTRAINT `FK_publications_id` FOREIGN KEY (`publication_id`) REFERENCES `publications` (`id`),
   ADD CONSTRAINT `fk_brand` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`);
 
@@ -306,7 +307,6 @@ ALTER TABLE `products`
 -- Filtros para la tabla `publications`
 --
 ALTER TABLE `publications`
-  ADD CONSTRAINT `fk_ori` FOREIGN KEY (`orientation_id`) REFERENCES `orientations` (`id`),
   ADD CONSTRAINT `publications_ibfk_1` FOREIGN KEY (`seller_id`) REFERENCES `users` (`id`);
 
 --
